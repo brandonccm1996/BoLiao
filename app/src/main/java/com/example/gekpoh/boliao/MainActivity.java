e8585cb9f43128e0e5c64b071b45cf32fe8d4b5f
Brandon <brandonccm@hotmail.com>
2018-06-04 17:54:03 +0800

    Added App Logo and Auth

--- app/src/main/java/com/example/gekpoh/boliao/MainActivity.java
+++ app/src/main/java/com/example/gekpoh/boliao/MainActivity.java
@@ -1,15 +1,70 @@
 package com.example.gekpoh.boliao;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // App Logo
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.common_google_signin_btn_icon_dark);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) { // user signed in
+
+                }
+                else {  // user signed out
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build()
+                                    ))
+                                    .build(), RC_SIGN_IN);
+                }
+            }
+        };
+    }
+<<<<<<< HEAD
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
 
+=======
+>>>>>>> 252d232e131c71d7a94e364206d37a66818ad1c9
 }
