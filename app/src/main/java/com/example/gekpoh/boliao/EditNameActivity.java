6acbb1978925ac3ab880a9c3aeff53c0ccca12fa
Brandon <brandonccm@hotmail.com>
2018-06-06 13:59:59 +0800

    Added EditName and EditDescription Activity

---
+++ app/src/main/java/com/example/gekpoh/boliao/EditNameActivity.java
@@ -0,0 +1,41 @@
+package com.example.gekpoh.boliao;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditNameActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private Button saveBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_name);
+
+        editTextName = findViewById(R.id.editTextName);
+        saveBtn = findViewById(R.id.saveBtn);
+
+        Intent intentThatStartedThisActivity = getIntent();
+        if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+            String currentName = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+            editTextName.setText(currentName);
+        }
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra(Intent.EXTRA_TEXT, editTextName.getText().toString());
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
+            }
+        });
+    }
+}
