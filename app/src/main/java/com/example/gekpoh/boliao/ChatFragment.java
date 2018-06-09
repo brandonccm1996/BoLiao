631b05c93438da097434ccceb513d6fa71dbfe3a
BJ <boon-jun1996@hotmail.com>
2018-06-09 12:29:21 +0800

    added textwatcher to edittext view in chat fragment

--- app/src/main/java/com/example/gekpoh/boliao/ChatFragment.java
+++ app/src/main/java/com/example/gekpoh/boliao/ChatFragment.java
@@ -5,10 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
 import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.database.FirebaseDatabase;
@@ -22,6 +25,8 @@ public class ChatFragment extends Fragment {
     private RecyclerView chatRecyclerView;
     private ArrayList<ChatMessage> chatMessageList;
     private ChatRecyclerAdapter adapter;
+    private EditText editText;
+    private Button sendButton;
     private FirebaseDatabase databaseReference;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,8 +38,33 @@ public class ChatFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mReference = getArguments().getString(getResources().getString(R.string.groupReferenceKey));
-        EditText editText = getView().findViewById(R.id.messageEditText);
+        editText = getView().findViewById(R.id.messageEditText);
         editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MAX_MESSAGE_LENGTH)});
+        sendButton = getView().findViewById(R.id.sendButton);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    sendButton.setEnabled(true);
+                } else {
+                    sendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         chatRecyclerView = getView().findViewById(R.id.chatRecyclerView);
         chatRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         adapter = new ChatRecyclerAdapter(chatMessageList);
