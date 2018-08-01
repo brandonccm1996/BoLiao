b01a00221c58eab92565d72bfa98436afe96e5de
BJ <boon-jun1996@hotmail.com>
2018-08-01 17:19:16 +0800

    A simple fix to connection loss issue after device went idling

--- app/src/main/java/com/example/gekpoh/boliao/StartActivity.java
+++ app/src/main/java/com/example/gekpoh/boliao/StartActivity.java
@@ -1,6 +1,7 @@
 package com.example.gekpoh.boliao;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 
@@ -17,7 +22,7 @@ public class StartActivity extends AppCompatActivity {
 
     private Button buttonSignIn;
     private static final int RC_SIGN_IN = 1;
-
+    private DatabaseReference ref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,16 +32,26 @@ public class StartActivity extends AppCompatActivity {
         getSupportActionBar().setLogo(R.drawable.logowhitesmall);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
+        ref = FirebaseDatabaseUtils.getDatabase().getReference().child("uselessString");
         buttonSignIn = findViewById(R.id.buttonSignIn);
 
         buttonSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
                 if (!FirebaseDatabaseUtils.connectedToDatabase()) {
                     Toasty.error(StartActivity.this, "Please check your internet connection", Toast.LENGTH_SHORT).show();
-                }
-                else {
+                } else {
                     startActivityForResult(
                             AuthUI.getInstance()
                                     .createSignInIntentBuilder()
